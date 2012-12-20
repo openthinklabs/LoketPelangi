b971bb15481ce7807f506b17cbd8893d3a29e225
Wildan Maulana <wildan.m@gmail.com>
2012-12-21 01:29:41 +0700

    First commit, skeleton POS Loket Pelangi

---
+++ apps/pos/config/posConfiguration.class.php
@@ -0,0 +1,8 @@
+<?php
+
+class posConfiguration extends sfApplicationConfiguration
+{
+  public function configure()
+  {
+  }
+}
---
+++ apps/pos/modules/pelanggan/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * pelanggan actions.
+ *
+ * @package    apps.pos.modules
+ * @subpackage pelanggan
+ * @author     Wildan Maulana, OpenThink Labs
+ */
+class pelangganActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  	
+  }
+}
